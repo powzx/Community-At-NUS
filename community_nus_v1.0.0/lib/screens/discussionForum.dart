8d5880e5aa8b2ec9a87e927b424d1b8784154dfd
changjianxiang <changjianxiang99@gmail.com>
2021-05-29 13:38:55 +0800

    Update discussionForum.dart

--- community_nus_v1.0.0/lib/screens/discussionForum.dart
+++ community_nus_v1.0.0/lib/screens/discussionForum.dart
@@ -38,7 +38,7 @@ class _DiscussionForumState extends State<DiscussionForum> {
                 builder: (BuildContext context, AsyncSnapshot userDetails) {
                   List<QueryDocumentSnapshot> listData = userDetails.data;
                   int userIdx = 0;
-                  for (int i = 0; i < userDetails.data.length; i++) {
+                  for (int i = 0; i < listData.length; i++) {
                     if (listData[i].id.toString().compareTo(this.uid) == 0) {
                       userIdx = i;
                     }
